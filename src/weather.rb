fbbdb90c5f45a9bf757e4ead95f9f4b656cc01ca
23prime <23.prime.37@gmail.com>
2018-06-06 14:15:51 +0900

    fix weather for the case: one of (min, max) is nil

--- src/weather.rb
+++ src/weather.rb
@@ -12,24 +12,31 @@ $city_all = JSON.parse(weather_json)
 
 def mk_weather(num)
   case num
+  # 0 -> today, 1 -> tomorrow.
+  # So, forecasts[0] -> today weather, forecasts[0] -> tomorrow weather.
   when 0, 1
-    city = 'つくば市'
+    city        = 'つくば市'
     day_weather = $city_all['forecasts'][num]
-    day = day_weather['dateLabel']
-    telop = day_weather['telop']
-    maxp = day_weather['temperature']['max']
-    minp = day_weather['temperature']['min']
 
-    if maxp == nil && minp == nil
-      return '> ' + city + 'の' + day + "の天気 <\n" + telop
-    else
-      maxc = maxp['celsius']
-      minc = minp['celsius']
-      saikou = '最高気温：'
-      saitei = '最低気温：'
-      saikou = '予想' + saikou if num == 1
-      saitei = '予想' + saitei if num == 1
-      return '> ' + city + 'の' + day + "の天気 <\n" + telop + "\n" + saikou + maxc + "℃\n" + saitei + minc + '℃'
+    day   = day_weather['dateLabel']  # 今日 or 明日
+    date  = day_weather['date'][8, 9] # yyyy-mm-dd -> dd
+    telop = day_weather['telop']      # example: 晴, 曇時々雨
+
+    min_temp    = day_weather['temperature']['min']
+    max_temp    = day_weather['temperature']['max']
+    min_celsius = ''
+    max_celsius = ''
+
+    unless min_temp == nil
+      min_celsius = '最低気温：' + min_temp['celsius'] + "℃"
+      min_celsius = '予想' + min_celsius if num == 1
     end
+
+    unless max_temp == nil
+      max_celsius = '最高気温：' + max_temp['celsius'] + "℃\n" if max_temp != nil
+      max_celsius = '予想' + max_celsius if num == 1
+    end
+
+    return '> ' + city + 'の' + day + '（' + date + "日）の天気 <\n" + telop + "\n" + max_celsius + min_celsius
   end
 end
