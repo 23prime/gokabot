14c44275786d1ca98f651e30e6e4375484b2f274
23prime <23.prime.37@gmail.com>
2018-06-09 00:09:13 +0900

    fix string concatenation

--- src/weather.rb
+++ src/weather.rb
@@ -11,10 +11,10 @@ def mk_weather(num, city)
   city_id = json[city]
 
   if city_id == nil
-    return '分かりませ〜んｗ'
+    return "分かりませ〜んｗ"
   else
     # Get weather info.
-    uri = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=' + city_id)
+    uri = URI.parse("http://weather.livedoor.com/forecast/webservice/json/v1?city=" + city_id)
     weather_json = Net::HTTP.get(uri)
     city_all = JSON.parse(weather_json)
 
@@ -22,30 +22,30 @@ def mk_weather(num, city)
     # 0 -> today, 1 -> tomorrow.
     # So, forecasts[0] -> today weather, forecasts[0] -> tomorrow weather.
     when 0, 1
-      day_weather = city_all['forecasts'][num]
+      day_weather = city_all["forecasts"][num]
 
-      day   = day_weather['dateLabel']  # 今日 or 明日
-      date  = day_weather['date'][8, 9] # yyyy-mm-dd -> dd
-      telop = day_weather['telop']      # example: 晴, 曇時々雨
+      day   = day_weather["dateLabel"]  # 今日 or 明日
+      date  = day_weather["date"][8, 9] # yyyy-mm-dd -> dd
+      telop = day_weather["telop"]      # example: 晴, 曇時々雨
 
-      min_temp    = day_weather['temperature']['min']
-      max_temp    = day_weather['temperature']['max']
-      min_celsius = ''
-      max_celsius = ''
+      min_temp    = day_weather["temperature"]["min"]
+      max_temp    = day_weather["temperature"]["max"]
+      min_celsius = ""
+      max_celsius = ""
 
-      unless min_temp == nil
-        min_celsius = "最低気温：" + min_temp['celsius'] + "℃"
-        min_celsius = '予想' + min_celsius if num == 1
-        min_celsius = "\n" + min_celsius
+      unless min_temp.nil?
+        min_celsius = "最低気温：#{min_temp["celsius"]}℃"
+        min_celsius = "予想#{min_celsius}" if num == 1
+        min_celsius = "\n#{min_celsius}"
       end
 
-      unless max_temp == nil
-        max_celsius = "最高気温：" + max_temp['celsius'] + "℃"
-        max_celsius = '予想' + max_celsius if num == 1
-        max_celsius = "\n" + max_celsius
+      unless max_temp.nil?
+        max_celsius = "最高気温：#{max_temp["celsius"]}℃"
+        max_celsius = "予想#{max_celsius}" if num == 1
+        max_celsius = "\n#{max_celsius}"
       end
 
-      return '> ' + city + 'の' + day + '（' + date + "日）の天気 <\n" + telop + max_celsius + min_celsius
+      return "> #{city}の#{day}（#{date}日）の天気 <\n#{telop}#{max_celsius}#{min_celsius}"
     end
   end
 end
