c50232f991aa2c154cddc5e92d3c7235197e1031
takenoko <ttaakkee@gmail.com>
2018-06-09 04:33:03 +0900

    Add Pixiv and Niconico searchers
    This is a combination of 8 commits.
    
    Add trigger patterns for Wikipedia searcher
    
    Improve extraction of abstract
    
    Abstract Wikipedia class
    
    Add Pixiv dictionary searcher
    
    Add WebDictsSearcher
    
    Add Niconico searcher
    
    Add not found messages
    
    Modularize

---
+++ src/web_dict/pixiv.rb
@@ -0,0 +1,19 @@
+require_relative "web_dict"
+
+module WebDict
+  class Pixiv < WebDict
+    def uri
+      return "https://dic.pixiv.net/a/"
+    end
+
+    private 
+
+    def first_elem_selector
+      return 'div.summary'
+    end
+
+    def read_further?(elem, count)
+      return count <= 0
+    end
+  end
+end
---
+++ src/web_dict/wikipedia.rb
@@ -0,0 +1,32 @@
+require_relative "web_dict"
+
+module WebDict
+  class Wikipedia < WebDict
+    def uri
+      return "https://ja.wikipedia.org/wiki/"
+    end
+
+    private 
+
+    def first_elem_selector
+      return 'div.mw-parser-output > p,' + 
+        'div.mw-parser-output > ul,' + 
+        'div.mw-parser-output > dl'
+    end
+
+    def filter_elem(elem, count)
+      remove_cites(elem)
+      return super(elem)
+    end
+
+    def read_further?(elem, count)
+      return ["p", "ul", "dl", "text"].include?(elem.name)
+    end
+
+    def remove_cites(elem)
+      elem.css('sup.reference').each do |e|
+        e.remove
+      end
+    end
+  end
+end
