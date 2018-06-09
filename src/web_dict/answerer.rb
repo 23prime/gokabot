d712f885be5510f4d7dea4d455b453dcf0291e82
takenoko <ttaakkee@gmail.com>
2018-06-10 02:40:46 +0900

    Fix Regexp

--- src/web_dict/answerer.rb
+++ src/web_dict/answerer.rb
@@ -3,7 +3,7 @@ require_relative 'pixiv'
 require_relative 'niconico'
 
 module WebDict
-  module Answerer
+  class Answerer
     WEB_DICTS = [
       Wikipedia.new(),
       Pixiv.new(),
@@ -15,7 +15,24 @@ module WebDict
       "そんなことも知らねえのかテメェは"
     ]
 
-    def self.search(keyword)
+    END_REG = /([\.\?．。？]|$)/
+    INTERROG_REG = /(って[\?？]|とは#{END_REG}|((とは|って)(なに|何|誰|だれ|どこ|((なん|何|誰|だれ|どこ)(なの|だよ|だょ|ですか|のこ))))#{END_REG})/
+    DELIM_REG = /[\.,．。，、]/
+    WORD_REG = /(([^\n\r\f\.,．。，、]+#{DELIM_REG}*)|#{DELIM_REG}+)/
+    NORMAL_QUESTION_REG = /(?<query>#{WORD_REG})#{INTERROG_REG}/
+    PREV_QUESTION_REG = /^#{INTERROG_REG}/
+
+    def reacts?(msg)
+      if NORMAL_QUESTION_REG =~ msg
+        @query = $~[:query]
+        return true
+      end
+      return false
+    end
+
+    attr_reader :query
+
+    def search(keyword)
       WEB_DICTS.each do |web_dict|
         result = web_dict.browse(keyword)
         return result unless result.nil?
@@ -23,12 +40,13 @@ module WebDict
       return nil
     end
 
-    def self.answer(keyword)
+    def answer(keyword = nil)
+      keyword = query if keyword.nil?
+      return NOT_FOUND_MESSAGES.sample() if keyword.nil?
       result = search(keyword)
-      unless result.nil?
-        return result
-      end
-      return NOT_FOUND_MESSAGES.sample()
+      return NOT_FOUND_MESSAGES.sample() if result.nil?
+      @query = nil
+      return result
     end
   end
 end
