0b6bba5ff2c40fa136bbb550fd4a4764267ff5e7
takenoko <ttaakkee@gmail.com>
2018-06-12 07:43:22 +0900

    Dont shuffle

--- src/web_dict/answerer.rb
+++ src/web_dict/answerer.rb
@@ -5,8 +5,8 @@ require_relative 'niconico'
 module WebDict
   class Answerer
     WEB_DICTS = [
-      Niconico.new(),
       Pixiv.new(),
+      Niconico.new(),
       Wikipedia.new(),
       WikipediaEN.new(),
     ]
@@ -30,12 +30,16 @@ module WebDict
     end
 
     def initialize()
-      @next_dicts = WEB_DICTS.shuffle
+      reset_next_dicts()
+    end
+
+    def reset_next_dicts()
+      @next_dicts = WEB_DICTS.dup
     end
 
     def search(keyword)
       threads = []
-      @next_dicts = WEB_DICTS.shuffle if keyword != @prev_keyword
+      reset_next_dicts() if keyword != @prev_keyword
       @prev_keyword = keyword
       @next_dicts.each do |web_dict|
         threads << Thread.start(keyword) do |keyword|
@@ -46,11 +50,11 @@ module WebDict
         result = thread.value()
         unless result.nil?
           @next_dicts.delete_at(i)
-          @next_dicts = WEB_DICTS.shuffle if @next_dicts.empty?
+          reset_next_dicts if @next_dicts.empty?
           return result 
         end
       end
-      @next_dicts = WEB_DICTS.shuffle
+      reset_next_dicts
       return nil
     end
 
