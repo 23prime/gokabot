721bf09787cf99b241b73b2ac23ad982f074fcfd
kasumiko <ponywoodcommon@gmail.com>
2018-09-14 17:02:11 +0900

    Add answer methods to some remaining classes
    Add answer methods to some remaining classes

--- src/nyokki.rb
+++ src/nyokki.rb
@@ -3,18 +3,23 @@
 class Nyokki
   @@nyokki_stat = 0
 
-  def self.nyokki(msg)
+  def nyokki(msg)
     @@nyokki_stat += 1
     if msg =~ /(ニョッキ|にょっき|ﾆｮｯｷ)/
     msg.tr!("０-９", "0-9")
     msg.delete!("^0-9")
-    return '' if msg.to_i == @@nyokki_stat
+    return nil if msg.to_i == @@nyokki_stat
     end
     @@nyokki_stat = 0
     "負けｗｗｗ"
   end
 
-  def self.stat
-    @@nyokki_stat
+  def answer(msg)
+    if @@nyokki_stat > 0 || msg =~ /(1|１)(にょっき|ニョッキ|ﾆｮｯｷ)/
+      nyokki(msg)
+    else
+      nil
+    end
   end
+
 end
