55470fb07190cf228068891a2fc3dcc5fcf64241
kasumiko <ponywoodcommon@gmail.com>
2018-09-15 12:04:30 +0900

    Fix TeX condition

--- src/texrenderer.rb
+++ src/texrenderer.rb
@@ -12,7 +12,7 @@ class Tex
   def answer(msg)
     if msg =~ /\$.+\$/ 
       return "日本語禁止" if msg =~/[^\x01-\x7E]/
-      msg.slice!(0).chop!
+      msg.chop!.slice!(0)
       return "長すぎだよ" if msg.length>=200
       $reply_type = 'image'
       request(msg)
