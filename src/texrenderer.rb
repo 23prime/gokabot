bdabda2fa46ee07ec25e8e71cf228214657f3d8f
kasumiko <ponywoodcommon@gmail.com>
2018-09-15 11:30:44 +0900

    Change TeX condition

--- src/texrenderer.rb
+++ src/texrenderer.rb
@@ -3,16 +3,17 @@ require "cgi"
 class Tex
 
   BASE_URI = "https://chart.googleapis.com/chart?cht=tx&chs=200&chl="
-  TEX_ID = /^tex /i
+
 
   def request(text)
     BASE_URI+CGI.escape(text)
   end
 
   def answer(msg)
-    if msg =~ TEX_ID
+    if msg =~ /\$.+\$/ 
       return "日本語禁止" if msg =~/[^\x01-\x7E]/
-      msg.slice!(TEX_ID)
+      msg.slice!(0).chop!
+      return "長すぎだよ" if msg.length>=200
       $reply_type = 'image'
       request(msg)
     else
