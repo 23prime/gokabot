2f76b53dd8d1219a0dbd31ffda16bad0a8d56606
23prime <23.prime.37@gmail.com>
2019-05-12 10:31:01 +0900

    Fix log print

--- app/main.rb
+++ app/main.rb
@@ -42,8 +42,8 @@ def reply(event)
   msg = event.message['text']
   user_id = event['source']['userId']
   name = get_name(user_id)
-  puts "Message: #{msg}"
   puts "From:    #{user_id} (#{name})"
+  puts "Message: #{msg}"
   return mk_reply(msg, user_id)
 end
 
