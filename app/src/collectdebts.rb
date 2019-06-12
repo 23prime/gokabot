6ff78e0b787898ce67b017bae0837bb41d3d2cda
kasumiko <ponywoodcommon@gmail.com>
2019-06-12 20:45:07 +0900

    Correct typo

--- app/src/collectdebts.rb
+++ app/src/collectdebts.rb
@@ -1,6 +1,6 @@
 class CollectDebts
   def takenoko?(*msg_data)
-    return 'お金返して' if msg_data[1] ~= /たけのこ/ || msg_data[2] == MY_USER_ID
+    return 'お金返して' if (msg_data[1] =~ /たけのこ/) || msg_data[2] == MY_USER_ID
   end
 
   def answer(*msg_data)
