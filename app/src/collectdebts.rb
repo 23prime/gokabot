1146ca14dd28faee54845aa08e1603ade8725228
kasumiko <ponywoodcommon@gmail.com>
2019-06-12 20:49:54 +0900

    Correct arguments numbers

--- app/src/collectdebts.rb
+++ app/src/collectdebts.rb
@@ -1,6 +1,6 @@
 class CollectDebts
   def takenoko?(*msg_data)
-    return 'お金返して' if (msg_data[1] =~ /たけのこ/) || msg_data[2] == MY_USER_ID
+    return 'お金返して' if  msg_data[1] == MY_USER_ID || (msg_data[2] =~ /たけのこ/)
   end
 
   def answer(*msg_data)
