129553cae4eed536f4bd3ea53b5ad688b7439ece
23prime <23.prime.37@gmail.com>
2019-06-17 00:48:02 +0900

    Add Main module and divide into two classes

--- app/main.rb
+++ app/main.rb
@@ -4,97 +4,134 @@ require 'dotenv/load'
 require 'rest-client'
 require_relative './src.rb'
 
-def reply(event)
-  $reply_type = 'text'
+module Main
+  class Reply
+    attr_accessor :body
 
-  msg = event.message['text']
-  user_id = event['source']['userId']
-  name = get_name(user_id)
-  reply_text = mk_reply_text(msg, user_id, name)
+    def initialize(event)
+      @event = event
+      @body = mk_reply
+    end
 
-  monitor(user_id, name, msg, reply_text)
-  reply = mk_reply_body(reply_text)
+    def mk_reply
+      $reply_type = 'text'
 
-  return reply
-end
+      msg = @event.message['text']
+      user_id = @event['source']['userId']
+      name = get_name(user_id)
+      reply_text = mk_reply_text(msg, user_id, name)
 
-def get_name(user_id)
-  token = ENV['LINE_CHANNEL_TOKEN']
-  uri = "https://api.line.me/v2/bot/profile/#{user_id}"
+      monitor(user_id, name, msg, reply_text)
+      reply = mk_reply_body(reply_text)
 
-  begin
-    res = RestClient.get uri, Authorization: "Bearer #{token}"
-    name = JSON.parse(res.body)['displayName']
-  rescue => exception
-    name = exception.message
-  end
+      return reply
+    end
 
-  return name
-end
+    def get_name(user_id)
+      token = ENV['LINE_CHANNEL_TOKEN']
+      uri = "https://api.line.me/v2/bot/profile/#{user_id}"
 
-def mk_reply_text(*msg_data)
-  reply_text = ''
-
-  $ANS_OBJS.each do |obj|
-    ans = obj.answer(*msg_data)
-
-    begin
-      unless ans.nil?
-        reply_text = ans
-        break
+      begin
+        res = RestClient.get uri, Authorization: "Bearer #{token}"
+        name = JSON.parse(res.body)['displayName']
+      rescue => exception
+        name = exception.message
       end
-    rescue => exception
-      exception.message
-      reply_text = "エラーおつｗｗｗｗｗｗ\n\n> #{exception}"
+
+      return name
+    end
+
+    def mk_reply_text(*msg_data)
+      reply_text = ''
+
+      $ANS_OBJS.each do |obj|
+        ans = obj.answer(*msg_data)
+
+        begin
+          unless ans.nil?
+            reply_text = ans
+            break
+          end
+        rescue => exception
+          exception.message
+          reply_text = "エラーおつｗｗｗｗｗｗ\n\n> #{exception}"
+        end
+      end
+
+      return reply_text
+    end
+
+    def monitor(user_id, name, msg, reply_text)
+      puts "From:    #{user_id} (#{name})"
+      puts "Message: #{msg}"
+      puts "Reply:   #{reply_text}"
+    end
+
+    def mk_reply_body(reply_text)
+      case $reply_type
+      when 'text'
+        return {
+          type: 'text',
+          text: reply_text
+        }
+      when 'image'
+        return {
+          type: 'image',
+          originalContentUrl: reply_text,
+          previewImageUrl: reply_text
+        }
+      else
+        return nil
+      end
+    end
+
+    def hello
+      msg = 'こん'
+
+      return {
+        type: 'text',
+        text: msg
+      }
     end
   end
 
-  return reply_text
-end
+  class Main
+    attr_accessor :client
 
-def monitor(user_id, name, msg, reply_text)
-  puts "From:    #{user_id} (#{name})"
-  puts "Message: #{msg}"
-  puts "Reply:   #{reply_text}"
-end
+    def initialize
+      @client ||= Line::Bot::Client.new { |config|
+        config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+        config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+      }
+    end
 
-def mk_reply_body(reply_text)
-  case $reply_type
-  when 'text'
-    return {
-      type: 'text',
-      text: reply_text
-    }
-  when 'image'
-    return {
-      type: 'image',
-      originalContentUrl: reply_text,
-      previewImageUrl: reply_text
-    }
-  else
-    return nil
+    def respond_to_events(events)
+      events.each { |event|
+        case event
+        when Line::Bot::Event::Message
+          case event.type
+          when Line::Bot::Event::MessageType::Text
+            reply = Reply.new(event)
+            @client.reply_message(event['replyToken'], reply.body)
+
+          when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+            response = @client.get_message_content(event.message['id'])
+            tf = Tempfile.open('content')
+            tf.write(response.body)
+          end
+
+        when Line::Bot::Event::Follow, Line::Bot::Event::Join, Line::Bot::Event::MemberJoined
+          @client.reply_message(event['replyToken'], Reply.hello)
+        end
+      }
+    end
   end
 end
 
-def hello
-  msg = 'こん'
-  reply = {
-    type: 'text',
-    text: msg
-  }
-
-  return reply
-end
-
-def client
-  @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-    config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-  }
-end
-
-# Execute
 post '/callback' do
+  main = Main::Main.new
+  client = main.client
+
   body = request.body.read
   signature = request.env['HTTP_X_LINE_SIGNATURE']
 
@@ -103,24 +140,6 @@ post '/callback' do
   end
 
   events = client.parse_events_from(body)
-
-  events.each { |event|
-    case event
-    when Line::Bot::Event::Message
-      case event.type
-      when Line::Bot::Event::MessageType::Text
-        client.reply_message(event['replyToken'], reply(event))
-
-      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-        response = client.get_message_content(event.message['id'])
-        tf = Tempfile.open('content')
-        tf.write(response.body)
-      end
-
-    when Line::Bot::Event::Follow, Line::Bot::Event::Join, Line::Bot::Event::MemberJoined
-      client.reply_message(event['replyToken'], hello)
-    end
-  }
-
+  main.respond_to_events(events)
   'OK'
 end
--- spec/app/main_spec.rb
+++ spec/app/main_spec.rb
@@ -1,23 +1,34 @@
 describe 'Reply test' do
-  let(:request_body) {
-    '{
-      "replyToken": "0f3779fba3b349968c5d07db31eab56f",
-      "type": "message",
-      "timestamp": 1462629479859,
-      "source": {
-        "type": "user",
-        "userId": "U0123456789abcdefghijklmnopqrstuv"
-      },
-      "message": {
-        "id": "325708",
-        "type": "text",
-        "text": "ぬるぽ"
-      }
+  let(:body) { '
+    {
+      "destination": "xxxxxxxxxx", 
+      "events": [
+        {
+          "replyToken": "0f3779fba3b349968c5d07db31eab56f",
+          "type": "message",
+          "timestamp": 1462629479859,
+          "source": {
+            "type": "user",
+            "userId": "U0123456789abcdefghijklmnopqrstuv"
+          },
+          "message": {
+            "id": "325708",
+            "type": "text",
+            "text": "ぬるぽ"
+          }
+        },
+        {
+          "replyToken": "8cf9239d56244f4197887e939187e19e",
+          "type": "follow",
+          "timestamp": 1462629479859,
+          "source": {
+            "type": "user",
+            "userId": "U4af4980629..."
+          }
+        }
+      ]
     }'
   }
-
-  let(:json) { JSON.parse(request_body) }
-  let(:event) { Line::Bot::Event::Message.new(json) }
 
   let(:true_reply) {
     {
@@ -27,10 +38,11 @@ describe 'Reply test' do
   }
 
   it 'NullPointer' do
-    # reply_text = mk_reply_text(msg, user_id, name)
-    # expect(reply_text).to eq 'ｶﾞｯ'
-
-    reply = reply(event)
-    expect(reply).to eq true_reply
+    body0 = Sinatra::Request.request.body.read
+    main = Main::Main.new
+    events = main.client.parse_events_from(body)
+    event = events[0]
+    reply = Main::Reply.new(event)
+    expect(reply.body).to eq true_reply
   end
 end
