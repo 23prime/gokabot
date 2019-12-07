9d23fde03ee1dcca9984b4bb7ea0f2d992ff6b8d
23prime <23.prime.37@gmail.com>
2019-12-07 21:29:03 +0900

    Add container restart script

---
+++ restart.sh
@@ -0,0 +1,26 @@
+option=''
+
+if [ $# -eq 0 ]; then
+    echo '##### git pull #####'
+    git pull
+
+    if [ `basename \`pwd\``  = 'gokabot-line-dev' ]; then
+        echo '##### Option set #####'
+        echo '-> For development'
+        option='-f docker-compose.debug.yml'
+    fi
+else
+    if [ $1 = '--local' ]; then
+        echo '##### Option set #####'
+        echo '-> For local'
+        option='-f docker-compose.debug.yml'
+    else
+        exit 1
+    fi
+fi
+
+echo '##### Restart Docker container #####'
+docker-compose $option down
+docker-compose $option build
+docker image prune -f
+docker-compose $option up -d
