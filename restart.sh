9f3a99448e31d63d5d3abb69f646f061a489722d
23prime <23.prime.37@gmail.com>
2020-02-09 23:54:32 +0900

    Run RSpec-test on Docker container

--- restart.sh
+++ restart.sh
@@ -1,19 +1,22 @@
 option=''
+container_name='gokabot-line'
 
 if [ $# -eq 0 ]; then
     echo '##### git pull #####'
     git pull --recurse-submodules
 
     if [ `basename \`pwd\``  = 'gokabot-line-dev' ]; then
-        echo '##### Option set #####'
+        echo '##### Set option #####'
         echo '-> For development'
         option='-f docker-compose.debug.yml'
+        container_name='gokabot-line-dev'
     fi
 else
     if [ $1 = '--local' ]; then
-        echo '##### Option set #####'
+        echo '##### Set option #####'
         echo '-> For local'
         option='-f docker-compose.local.yml'
+        container_name='gokabot-line-local'
     else
         exit 1
     fi
@@ -24,3 +27,8 @@ docker-compose $option down
 docker-compose $option build
 docker image prune -f
 docker-compose $option up -d
+
+echo '##### Run RSpec-test on Docker container #####'
+docker exec -it $container_name bundle exec rspec
+
+echo '##### Finish! #####'
