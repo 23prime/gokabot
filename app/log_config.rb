7089d93c6f677ad4a53cd9ab74656c62c2103ba5
23prime <23.prime.37@gmail.com>
2020-03-24 17:23:32 +0900

    Set log formatter

--- app/log_config.rb
+++ app/log_config.rb
@@ -2,4 +2,8 @@ require 'logger'
 
 module LogConfig
   @@logger = Logger.new(STDOUT)
+  datetime_format = '%Y-%m-%d %H:%M:%S.%03N'
+  @@logger.formatter = proc do |severity, datetime, _progname, msg|
+    "[#{datetime.strftime(datetime_format)}] #{severity}\t#{msg}\n"
+  end
 end
