4d08b91738a5a30aaf13d262955fe6d7e537929e
23prime <23.prime.37@gmail.com>
2020-04-22 19:51:44 +0900

    Add pushing endpoints to product

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'dotenv/load'
 require 'rest-client'
+
+require './app/src/push/push'
+require './app/src/push/ramdom_push'
 require_relative './response.rb'
 
 get '/' do
@@ -12,4 +15,17 @@ post '/callback' do
   signature = request.env['HTTP_X_LINE_SIGNATURE']
   Response::Response.response(body, signature)
   'OK'
+end
+
+post '/push' do
+  msg = params[:msg]
+  target = params[:target]
+  return 401 if msg.nil? || target.nil?
+  return Push.send_push_msg(msg, target)
+end
+
+post '/push/random' do
+  target = params[:target]
+  return 401 if target.nil?
+  return RamdomPush.send_push_msg(target)
 end
