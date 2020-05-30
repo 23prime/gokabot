203adb51fdf523efb6b3032886d9b3e06feadc6d
23prime <23.prime.37@gmail.com>
2020-05-30 23:45:49 +0900

    Remove 'A91' from test case

--- spec/app/src/searchdolls_spec.rb
+++ spec/app/src/searchdolls_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper'
 
 test_cases = [
-  ['A91',  'https://cdn.wikiwiki.jp/to/w/dolls-fl/A91/::ref/A91.jpg'],
-  ['damage A91', 'https://cdn.wikiwiki.jp/to/w/dolls-fl/A91/::ref/A91_damage.jpg'],
   ['Ak 5', 'https://cdn.wikiwiki.jp/to/w/dolls-fl/Ak%205/::ref/Ak%205.jpg'],
   ['03式', 'https://cdn.wikiwiki.jp/to/w/dolls-fl/03%E5%BC%8F/::ref/03%E5%BC%8F.jpg'],
-  ['BGM',  '該当するドールが見つかりません'],
-  ['hoge', '該当するドールが見つかりません']
+  %w[BGM 該当するドールが見つかりません],
+  %w[hoge 該当するドールが見つかりません]
 ]
 
 describe DflSearch do
@@ -18,7 +16,7 @@ describe DflSearch do
 
     it msg do
       ans = dfl_search.answer(msg)
-      expect(exp_ans).to eq ans
+      expect(ans).to eq exp_ans
     end
   end
 end
