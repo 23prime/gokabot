36894a072d745df0c9984794548fbe7e2326ca03
23prime <23.prime.37@gmail.com>
2020-06-14 13:50:33 +0900

    Swap severity and program name on log

--- app/log_config.rb
+++ app/log_config.rb
@@ -4,6 +4,6 @@ module LogConfig
   @@logger = Logger.new(STDOUT)
   datetime_format = '%Y-%m-%d %H:%M:%S.%03N'
   @@logger.formatter = proc do |severity, datetime, progname, msg|
-    "[#{datetime.strftime(datetime_format)}] [#{progname}] [#{severity}] #{msg}\n"
+    "[#{datetime.strftime(datetime_format)}] [#{severity}] [#{progname}] #{msg}\n"
   end
 end
