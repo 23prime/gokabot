16b1671df549e3d3e0a4e1f910b42f0c80538bee
23prime <23.prime.37@gmail.com>
2021-01-16 21:29:50 +0900

    Fix aws cli command

--- gokabot-demo/deploy-to-s3.sh
+++ gokabot-demo/deploy-to-s3.sh
@@ -1,4 +1,4 @@
-aws2 s3 cp dist/index.html s3://gokabot-demo
-aws2 s3 cp dist/favicon.ico s3://gokabot-demo
-aws2 s3 cp dist/css s3://gokabot-demo/css --recursive
-aws2 s3 cp dist/js s3://gokabot-demo/js --recursive
+aws s3 cp dist/index.html s3://gokabot-demo
+aws s3 cp dist/favicon.ico s3://gokabot-demo
+aws s3 cp dist/css s3://gokabot-demo/css --recursive
+aws s3 cp dist/js s3://gokabot-demo/js --recursive
