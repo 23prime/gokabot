19f12d295c056d0bc3dc74c2775ac0ed5153024e
23prime <23.prime.37@gmail.com>
2022-06-18 15:30:45 +0900

    Add quote rule

--- gokabot-demo/.eslintrc.js
+++ gokabot-demo/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        quotes: ["error", "double"],
         "vue/script-indent": ["error", 4, { baseIndent: 0 }],
         "vue/multi-word-component-names": 0,
     },
