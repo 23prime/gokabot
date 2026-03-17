abf67bf9f805458cc262f88928dd4ab4186fad99
23prime <23.prime.37@gmail.com>
2026-03-17 23:30:21 +0900

    chore: Add custom domain api.gokabot.com to Lightsail container service
    - Lightsail SSL certificate for api.gokabot.com
    - Route 53 DNS validation CNAME records (two-step apply required)
    - public_domain_names block to attach cert to container service
    - Route 53 CNAME api.gokabot.com → container service URL

--- infrastructure/lightsail/main.tf
+++ infrastructure/lightsail/main.tf
@@ -8,12 +8,52 @@ terraform {
   }
 }
 
+data "aws_route53_zone" "gokabot" {
+  name = "gokabot.com"
+}
+
+resource "aws_lightsail_certificate" "api" {
+  name        = "gokabot-api-cert"
+  domain_name = "api.gokabot.com"
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_lightsail_certificate.api.domain_validation_options : dvo.domain_name => {
+      name  = dvo.resource_record_name
+      type  = dvo.resource_record_type
+      value = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.gokabot.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.value]
+  ttl     = 60
+}
+
 resource "aws_lightsail_container_service" "gokabot_api" {
   name  = "gokabot-api"
   power = "nano"
   scale = 1
 
+  public_domain_names {
+    certificate {
+      certificate_name = aws_lightsail_certificate.api.name
+      domain_names     = ["api.gokabot.com"]
+    }
+  }
+
   tags = {
     Project = "gokabot"
   }
+}
+
+resource "aws_route53_record" "api" {
+  zone_id = data.aws_route53_zone.gokabot.zone_id
+  name    = "api.gokabot.com"
+  type    = "CNAME"
+  ttl     = 300
+  records = [trimsuffix(trimprefix(aws_lightsail_container_service.gokabot_api.url, "https://"), "/")]
 }
