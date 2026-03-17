b8a2df935fc6ac92fe1d03bd76c4a4d57684e007
23prime <23.prime.37@gmail.com>
2026-03-17 15:43:48 +0900

    chore: Add Terraform/Terragrunt infrastructure for Lightsail and GitHub OIDC

---
+++ infrastructure/github-oidc/main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.github_actions.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:23prime/gokabot:ref:refs/heads/main"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "gokabot-github-actions"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+
+  tags = {
+    Project = "gokabot"
+  }
+}
+
+data "aws_iam_policy_document" "lightsail_push" {
+  statement {
+    actions = [
+      "lightsail:PushContainerImage",
+      "lightsail:RegisterContainerImage",
+      "lightsail:GetContainerImages",
+      "lightsail:GetContainerServiceDeployments",
+      "lightsail:CreateContainerServiceDeployment",
+    ]
+    resources = [
+      "arn:aws:lightsail:*:${data.aws_caller_identity.current.account_id}:ContainerService/gokabot-api",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "lightsail_push" {
+  name   = "lightsail-push"
+  role   = aws_iam_role.github_actions.id
+  policy = data.aws_iam_policy_document.lightsail_push.json
+}
